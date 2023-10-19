a0a75e62a9cd58b665119d87d58ae5e943f67d25
Jake <jbarber@gfxlabs.io>
2023-10-19 16:51:36 -0500

    it compiles now

--- contracts/contracts/IERC20.sol
+++ contracts/contracts/IERC20.sol
@@ -3,12 +3,7 @@ pragma solidity ^0.8.9;
 
 interface IERC20 {
     function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) public returns (bool success);
+    function transferFrom(address from, address to, uint256 value) external returns (bool success);
     function balanceOf(address account) external view returns (uint256);
-    function approve(address spender, uint256 value) public returns (bool success);
-}
-
-interface IWETH is IERC20 {
-    function deposit() external payable;
-    function withdraw(uint amount) external;
+    function approve(address spender, uint256 value) external returns (bool success);
 }
---
+++ contracts/contracts/TokenBridge.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.9;
+
+import "./IWETH.sol";
+import "./IWormhole.sol";
+
+contract TokenBridge {
+    
+}
